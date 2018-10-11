5e6fa829cb95b387f3d9f883098ee94f1ae89dd5
allerria <petrov-georgiy@mail.ru>
2018-10-11 14:24:49 +0900

    Initial commit

---
+++ desktop/src/com/test/solar_system/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.test.solar_system.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.test.solar_system.SolarSystemGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SolarSystemGame(), config);
+	}
+}
